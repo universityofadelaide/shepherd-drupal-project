9d05ed7cf0124656fa5a55d038a1c3d2804049b2
caseyfw <casey.fulton@adelaide.edu.au>
2017-03-10 15:25:11 +1030

    ScriptHandler adds env var parsing and inclusion of local settings to settings.php.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -42,6 +42,35 @@ class ScriptHandler {
       $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
     }
 
+    // Append Shepherd-specific environment variable settings to settings.php.
+    file_put_contents(
+      $root . '/sites/default/settings.php',
+      "\n/**\n * START DATABASE CONFIG \n */\n" .
+      "\$databases['default']['default'] = array (\n" .
+      "  'database' => getenv('DATABASE_NAME'),\n" .
+      "  'username' => getenv('DATABASE_USER'),\n" .
+      "  'password' => (getenv('DATABASE_PASSWORD_FILE') ? file_get_contents(getenv('DATABASE_PASSWORD_FILE')) : getenv('DATABASE_PASSWORD')),\n" .
+      "  'host' => getenv('DATABASE_HOST'),\n" .
+      "  'port' => (getenv('DATABASE_PORT') ?: '3306'),\n" .
+      "  'driver' => (getenv('DATABASE_DRIVER') ?: 'mysql'),\n" .
+      "  'prefix' => getenv('DATABASE_PREFIX'),\n" .
+      "  'collation' => (getenv('DATABASE_COLLATION') ?: 'utf8mb4_general_ci'),\n" .
+      "}\n" .
+      "/**\n * END DATABASE CONFIG \n */\n\n",
+      FILE_APPEND
+    );
+
+    // Append inclusion of settings.local.php to settings.php.
+    file_put_contents(
+      $root . '/sites/default/settings.php',
+      "/**\n * START LOCAL CONFIG \n */\n" .
+      "if (file_exists(__DIR__ . '/settings.local.php')) {\n" .
+      "  include __DIR__ . '/settings.local.php';\n" .
+      "}\n" .
+      "/**\n * END LOCAL CONFIG \n */\n\n",
+      FILE_APPEND
+    );
+
     // Prepare the services file for installation
     if (!$fs->exists($root . '/sites/default/services.yml') and $fs->exists($root . '/sites/default/default.services.yml')) {
       $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
